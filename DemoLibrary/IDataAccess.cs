23379c7613abbe1fcdcea09485e98170048515aa
alexandr digilevich <digilevich.alexandr@gmail.com>
2021-11-03 18:58:32 +0300

    Add project files.

---
+++ DemoLibrary/IDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using DemoLibrary.Models;
+using System.Collections.Generic;
+
+namespace DemoLibrary
+{
+    public interface IDataAccess
+    {
+        List<PersonModel> GetPeople();
+        PersonModel InsertPerson(string firstName, string lastName);
+    }
+}
---
+++ DemoLibrary/List.cs
@@ -0,0 +1,6 @@
+﻿namespace DemoLibrary
+{
+    internal class List<T1, T2>
+    {
+    }
+}
